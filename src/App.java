387333ea80edb07084a60b5dd9f5ac1ca3e5b883
mutteripannu <179133918+mutteripannu@users.noreply.github.com>
2024-10-06 15:42:18 +0300

    Stage one

--- src/App.java
+++ src/App.java
@@ -2,7 +2,18 @@ import java.util.Random;
 public class App {
     public static void main(String[] args) throws Exception {
         Random random = new Random();
-        int randomNumber = random.nextInt(10)+1;
-        System.out.println("Random number: " + randomNumber);
+        int randomNumber01 = random.nextInt(10)+1;
+        int randomNumber02 = random.nextInt(10)+1;
+        int randomNumber03 = random.nextInt(10)+1;
+        System.out.println("Random number: " + randomNumber01);
+        System.out.println("Random number: " + randomNumber02);
+        System.out.println("Random number: " + randomNumber03);
+
+        if(randomNumber01 == 7 || randomNumber02 == 7 || randomNumber03 == 7){
+            System.out.println("You're a winner!!!");
+        }
+        else{
+            System.out.println("You lost...");
+        }
     }
 }
